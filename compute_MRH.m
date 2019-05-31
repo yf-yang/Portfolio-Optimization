a366bc8ade5282f6e17ffbf6164b34e8537820f6
yf-yang <directoryyf@gmail.com>
2019-05-30 17:27:42 -0700

    remove matlab implementation
    Signed-off-by: yf-yang <directoryyf@gmail.com>

--- compute_MRH.m
+++
@@ -1,9 +0,0 @@
-function [ Sigma_MRH, F_MRH, R_MRH ] = compute_MRH( Sigma_SAM, K)
-
-M = size( Sigma_SAM, 1 );
-[ eigvec, eigval ] = order_eig(Sigma_SAM);
-sigma2 = mean( eigval(K+1:M) );
-
-F_MRH = eigvec(:,1:K) * diag(eigval(1:K)-sigma2) * eigvec(:,1:K)';
-R_MRH = diag( diag(Sigma_SAM)-diag(F_MRH) );
-Sigma_MRH = F_MRH + R_MRH;
--- logdet.m
+++
@@ -1,7 +0,0 @@
-function ld = logdet( Sigma )    
-[ L, p] = chol( Sigma );
-if p~= 0 % not PSD
-    ld = -inf;
-else
-    ld = sum(log(diag(L)))*2;
-end
